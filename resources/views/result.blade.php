eac43d65e509a4aa7ae05758fd348d508ced9726
c3budiman <c3budiman@gmail.com>
2018-09-05 12:14:07 +0700

    rapiin bug, sama nambahin klo email nya kosong otomatis ngambil dari ss

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -19,21 +19,35 @@
         if ($npmDicari == "") {
           continue;
         }
+        $url = "http://".$npmDicari.".student.gunadarma.ac.id/tugas.html";
+    		$ch = curl_init();
+    		curl_setopt($ch, CURLOPT_URL, $url);
+    		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    		$response = curl_exec($ch);
+    		if (curl_errno($ch)) die(curl_error($ch));
+    		curl_close($ch);
+    		$re = '/<tr><td><i><b>Email<\/b><\/td><td>:(.*)\[/m';
+    		preg_match_all($re, $response, $matches, PREG_SET_ORDER, 0);
         ?>
         <tr>
-          <td>{{DB::table('dbf2')->where('NPM','=',$npmDicari)->get()->first()->NPM}}</td>
+          <td>{{$npmDicari}}</td>
           <td>{{DB::table('dbf2')->where('NPM','=',$npmDicari)->get()->first()->NAMA}}</td>
           <td>{{DB::table('dbf2')->where('NPM','=',$npmDicari)->get()->first()->TGL_LULUS}}</td>
           <td>{{DB::table('dbf2')->where('NPM','=',$npmDicari)->get()->first()->TGL_LAHIR}}</td>
           <td>{{DB::table('dbf2')->where('NPM','=',$npmDicari)->get()->first()->TEMPAT_LAHIR}}</td>
           <td>{{substr(DB::table('dbf2')->where('NPM','=',$npmDicari)->get()->first()->JALAN,0,30)}}</td>
-          <td>{{substr(DB::table('dbf2')->where('NPM','=',$npmDicari)->get()->first()->JALAN,31)}}</td>
+          <td>{{substr(DB::table('dbf2')->where('NPM','=',$npmDicari)->get()->first()->JALAN,30)}}</td>
           <td>{{DB::table('dbf2')->where('NPM','=',$npmDicari)->get()->first()->RT}}</td>
           <td>{{DB::table('dbf2')->where('NPM','=',$npmDicari)->get()->first()->RW}}</td>
           <td>{{DB::table('dbf2')->where('NPM','=',$npmDicari)->get()->first()->KOTA}}</td>
           <td>{{DB::table('dbf2')->where('NPM','=',$npmDicari)->get()->first()->KODEPOS}}</td>
           <td>{{DB::table('dbf2')->where('NPM','=',$npmDicari)->get()->first()->HP}}</td>
-          <td>{{DB::table('dbf2')->where('NPM','=',$npmDicari)->get()->first()->EMAIL}}</td>
+          @if (DB::table('dbf2')->where('NPM','=',$npmDicari)->get()->first()->EMAIL)
+            <td>{{DB::table('dbf2')->where('NPM','=',$npmDicari)->get()->first()->EMAIL}}</td>
+          @else
+            <td>{{substr($matches[0][1],1,strlen($matches[0][1])-2)."@student.gunadarma.ac.id"}}</td>
+          @endif
+
         </tr>
         <?php
       }
